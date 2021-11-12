29b209bc144c0003cacda0602860975e21a9565e
RykAlex <yes45@mail.ru>
2021-11-12 15:55:44 +0300

    Header auth V1

--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -1,43 +1,28 @@
-let open = document.querySelector(".dropbtn");
-let arrow = document.querySelector(".icon-arrow");
+// let open = document.querySelector(".dropbtn");
+// let arrow = document.querySelector(".icon-arrow");
 
-// open.forEach(P);
+// open.addEventListener("click", function () {
+//   document.getElementById("btnInfo").classList.toggle("show");
+//   arrow.classList.toggle("active");
+// });
 
-// function P(item) {
-//   item.addEventListener("click", function () {
-//     document.getElementById("btnInfo").classList.toggle("show");
-//   });
-// }
-// let arrow = document.querySelectorAll(".icon-arrow");
-// arrow.forEach(T);
+// window.addEventListener("click", function (event) {
+//   if (!event.target.matches(".dropbtn")) {
+//     let dropdowns = document.getElementsByClassName("dropdown-content");
+//     for (i = 0; i < dropdowns.length; i++) {
+//       let openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains("show")) {
+//         openDropdown.classList.remove("show");
+//         arrow.classList.remove("active");
+//       }
+//     }
+//   }
+// });
+let open = document.querySelectorAll(".dropbtn");
 
-// function T(item) {
-//   item.addEventListener("click", function () {
-//     this.classList.toggle("active");
-//   });
-// }
-// // arrow.forEach(T);
-
-// // function T(item) {
-// //   item.addEventListener("click", function () {
-// //     arrow.classList.toggle("active");
-// //   });
-// // }
-
-open.addEventListener("click", function () {
-  document.getElementById("btnInfo").classList.toggle("show");
-  arrow.classList.toggle("active");
-});
-
-document.addEventListener("click", function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-        arrow.classList.remove("active");
-      }
-    }
-  }
-});
+open.forEach(Fn);
+function Fn(item) {
+  item.addEventListener("click", function () {
+    this.nextElementSibling.classList.toggle("show");
+  });
+}
