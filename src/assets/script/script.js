6a03bbd10fa3fe1863f34b8217f118ba95d526ba
RykAlex <yes45@mail.ru>
2021-11-17 16:23:49 +0300

    Header Menu v1

--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -1,3 +1,43 @@
+let SimpleBar = require("simplebar");
+
+// Скрипт для header no auth
+// ============================================
+let openBtn = document.querySelector(".btn-retail");
+let arrowBtn = document.querySelector(".auth-arrow");
+
+openBtn.addEventListener("click", function () {
+  document.getElementById("authBtn").classList.toggle("show");
+  arrowBtn.classList.toggle("active");
+});
+window.addEventListener("click", function (event) {
+  if (!event.target.matches(".btn-retail")) {
+    let openDropdown = document.querySelector(".submenu");
+    if (openDropdown.classList.contains("show")) {
+      openDropdown.classList.remove("show");
+      arrowBtn.classList.remove("active");
+    }
+  }
+});
+
+// Скрипт для Menu
+// ============================================
+let openMenu = document.querySelector(".header-items-no-auth .menu-icon");
+let openMenuAuth = document.querySelector(".header-items-auth .menu-icon");
+// let menuMobile = document.querySelector(".header-items-no-auth .menu-icon");
+let menuNoAuth = document.querySelector(".menu-noauth");
+let menuAuth = document.querySelector(".menu-auth");
+
+openMenu.addEventListener("click", function () {
+  menuNoAuth.classList.toggle("show");
+});
+
+openMenuAuth.addEventListener("click", function () {
+  menuAuth.classList.toggle("show");
+});
+
+// menuMobile.addEventListener("click", function () {
+//   menuNoAuth.classList.toggle("show");
+// });
 // let open = document.querySelector(".dropbtn");
 // let arrow = document.querySelector(".icon-arrow");
 
@@ -20,16 +60,16 @@
 // });
 let open = document.querySelectorAll(".dropbtn");
 let arrow = document.querySelectorAll(".icon-arrow");
+
 open.forEach(Fn);
 function Fn(item) {
   item.addEventListener("click", function () {
-    this.nextElementSibling.classList.toggle("show");
-
-    arrow.forEach(Fa);
-    function Fa(item) {
-      item.addEventListener("click", function () {
-        this.classList.toggle("active");
-      });
+    item.nextElementSibling.classList.toggle("show");
+    for (let i = 0; i < arrow.length; i++) {
+      const element = arrow[i];
+      if (element.classList.contains("icon-arrow")) {
+        element.classList.toggle("active");
+      }
     }
   });
 }
