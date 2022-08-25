6e28f72d3fce952696dce86acf6c1a39e20c21c5
RykAlex <yes45@mail.ru>
2022-08-25 13:40:29 +0300

    Page All pop up

--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function (event) {
 
+document.addEventListener('DOMContentLoaded', function (event) {
   let SimpleBar = require("simplebar");
 
   btnIconArrow();
